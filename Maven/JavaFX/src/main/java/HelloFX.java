239a46aeb0c19f8151ca4f75401e1a28edc3d770
Danil-Didkovskiy <danylo.didkovskyi@teamdev.com>
2024-01-26 21:43:42 +0200

    Replace `html5test.co` with `html5test.teamdev.com`

--- Maven/JavaFX/src/main/java/HelloFX.java
+++ Maven/JavaFX/src/main/java/HelloFX.java
@@ -43,7 +43,7 @@ public final class HelloFX extends Application {
         Browser browser = engine.newBrowser();
 
         // Load the required web page.
-        browser.navigation().loadUrl("https://html5test.co");
+        browser.navigation().loadUrl("https://html5test.teamdev.com");
 
         // Create and embed JavaFX BrowserView component to display web content.
         BrowserView view = BrowserView.newInstance(browser);
